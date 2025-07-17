27ba7ce01b9c70843fd3cf4d4764594a0c214c52
chronopio <joaquinalejandro00@gmail.com>
2025-07-17 11:49:13 -0400

    feat: add support for holonym wasm on build config

--- apps/extension/vite.popup.config.ts
+++ apps/extension/vite.popup.config.ts
@@ -1,14 +1,26 @@
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
+import wasm from 'vite-plugin-wasm';
+import topLevelAwait from 'vite-plugin-top-level-await';
 
 export default defineConfig({
+    plugins: [wasm(), topLevelAwait()],
     build: {
         rollupOptions: {
             input: {
-                popup: resolve(__dirname, 'popup/popup.ts')
+                popup: resolve(__dirname, 'popup/popup.ts'),
+                holonymCallback: resolve(
+                    __dirname,
+                    'popup/holonym/holonym-callback.ts'
+                )
             },
             output: {
-                entryFileNames: 'popup/[name].js'
+                entryFileNames: (chunkInfo) => {
+                    if (chunkInfo.name === 'holonymCallback') {
+                        return 'popup/holonym/holonym-callback.js';
+                    }
+                    return 'popup/[name].js';
+                }
             }
         },
         outDir: './dist',
